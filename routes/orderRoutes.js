2132a44e3b7d85734dab4ce14770b5c92723c5d3
Mr-Jeramz <njeramson@gmail.com>
2026-03-07 20:16:26 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,23 +1,12 @@
 // Order Routes - Define all order-related routes
 
-const express = require('express');
+cconst express = require("express");
 const router = express.Router();
-const orderController = require('../controllers/orderController');
 
-// GET /api/orders - Get all orders
-router.get('/', orderController.getAllOrders);
+const orderController = require("../controllers/orderController");
 
-// GET /api/orders/:id - Get single order
-router.get('/:id', orderController.getOrderById);
-
-// POST /api/orders - Create new order
-router.post('/', orderController.createOrder);
-
-// PUT /api/orders/:id - Update order (status)
-router.put('/:id', orderController.updateOrder);
-
-// DELETE /api/orders/:id - Delete order
-router.delete('/:id', orderController.deleteOrder);
+router.get("/", orderController.getOrders);
+router.post("/", orderController.createOrder);
+router.put("/:id", orderController.updateOrderStatus);
 
 module.exports = router;
-
