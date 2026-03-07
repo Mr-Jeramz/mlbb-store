9c3a35d030ff2289d7fa43e9debc3f74475e3fec
Mr-Jeramz <njeramson@gmail.com>
2026-03-07 21:01:52 +0530

    Update statsRoutes.js

--- routes/statsRoutes.js
+++ routes/statsRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 const statsController = require("../controllers/statsController");
 
+// Get dashboard statistics
 router.get("/", statsController.getStats);
 
 module.exports = router;
