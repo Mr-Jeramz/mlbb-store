c3373ed7342d1a0a147bfbabf40d6877a7c12dfe
Mr-Jeramz <njeramson@gmail.com>
2026-03-07 23:06:05 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const orderController = require("../controllers/orderController");
 
 router.get("/", orderController.getAllOrders);
-router.get("/:id", orderController.getAllOrderById);
+router.get("/:id", orderController.getOrderById);
 router.post("/", orderController.createOrder);
 router.put("/:id", orderController.updateOrder);
 router.delete("/:id", orderController.deleteOrder);
