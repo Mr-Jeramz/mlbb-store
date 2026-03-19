c19cd250cb5116264cfa7384a3efd608c6956b9b
Mr-Jeramz <njeramson@gmail.com>
2026-03-19 20:15:58 +0530

    Update accountRoutes.js

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
 const accountController = require("../controllers/accountController");
-
 router.get("/", accountController.getAccounts);
 router.post("/", accountController.createAccount);
-
+router.delete("/:id", accountController.deleteAccount);
 module.exports = router;
